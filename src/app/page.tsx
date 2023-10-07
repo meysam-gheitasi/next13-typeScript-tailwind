0485478f46057c2dcc83f90ccdbf8e2820b474dc
meysam1920 <121888134+meysam1920@users.noreply.github.com>
2023-10-07 14:59:23 +0330

    Package function shadcn test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
-    <div className=" bg-slate-500">Home</div>
+    <div className=" bg-slate-500">Home
+    <Button>Click me</Button>
+    </div>
   )
 }
